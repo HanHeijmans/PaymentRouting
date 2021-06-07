3821e086b92772c143f2189b24f1a2c5292604a9
Han Heijmans <h.a.heijmans@student.tudelft.nl>
2021-06-07 16:15:03 +0200

    Simultations

---
+++ out/artifacts/PaymentRoutingEvaluate_jar/script.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import time
+
+prc = []
+
+for n in range(11):
+    prc.append(subprocess.Popen(['java', '-jar', 'PaymentRouting.jar', str(n)]))
+
+count = len(prc)
+runs = 11
+while True:
+    a = False
+    for n, p in enumerate(prc):
+        if p.poll() != 0:
+            a = True
+        elif count < runs:
+            prc[n] = subprocess.Popen(['java', '-jar', 'PaymentRouting.jar', str(count)])
+            count += 1
+    if not a and count >= runs:
+        print('Done')
+        break;
+    time.sleep(1)
+
+
+
+    
